8b2b69859b82c5de3764a957302401b7668438ef
trijal-singh-babbar <trijalsinghbabbar@gmail.com>
2025-03-05 19:34:25 +0530

    Created Deck class

---
+++ Deck.java
@@ -0,0 +1,34 @@
+// Deck.java
+
+import java.util.*;
+class Deck
+{
+	ArrayList<Card> deck;
+	
+	public Deck()
+	{
+		deck = new ArrayList<>();
+	}
+	
+	public void createDeck()
+	{
+		String[] ranks = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+		String[] suits = {"Spades","Hearts","Diamonds","Clubs"};
+		
+		for(String rank : ranks)
+		{
+			for(String suit : suits)
+			{
+				deck.add(new Card(rank, suit));
+			}
+		}
+	}
+	
+	public void displayDeck()
+	{
+		for(Card card : deck)
+		{
+			System.out.println(card);
+		}
+	}
+}

0242d026c3eaa003348475d4d5ffc1653e87b625
trijal-singh-babbar <trijalsinghbabbar@gmail.com>
2025-03-05 19:42:07 +0530

    Created class Card

---
+++ Card.java
@@ -0,0 +1,18 @@
+// Card.java
+
+class Card
+{
+	String rank;
+	String suit;
+	
+	public Card(String rank, String suit)
+	{
+		this.rank = rank;
+		this.suit = suit;
+	}
+	
+	public String toString()
+	{
+		return rank+" of " +suit;
+	}
+}
